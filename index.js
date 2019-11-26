19235e1c7f328093c4283348910ce125e95b5c89
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2019-11-26 01:22:03 -0500

    simplify

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function handleRequest(request) {
   const width = size ? size : (params.get('width') || params.get('w'))
   const scale = params.get('scale') === '' || !!params.get('scale')
 
-  let svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"'
+  let svg = '<svg xmlns="http://www.w3.org/2000/svg"'
 
   if (!scale) {
     svg += ` width="${ width }"`
